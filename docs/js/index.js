7907f74d4826bcbe3ef59a59a103cb5f0971bfec
Sho-Um <73683252+Sho-Um@users.noreply.github.com>
2021-04-10 10:19:11 +0900

    ハンバーガーバーの仮置き

--- docs/js/index.js
+++ docs/js/index.js
@@ -25,3 +25,16 @@ $(window).on('load',function(){
     });
     //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる    
 });
+
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $("#header,#container").toggleClass('mainblur');//ぼかしたいエリアにmainblurクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去し
+    $("#header,#container").removeClass('mainblur');//ぼかしているエリアのmainblurクラスを除去
+});
